b2f44b59e9b1b6a6aecf930dc6228c5e1c69931e
ngmt <ngmt83ngmt@gmail.com>
2019-06-02 20:05:58 +0900

    描画タイミングの異なるバーコードを追加

--- src/app/barcode/barcode.component.ts
+++ src/app/barcode/barcode.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
+import * as JsBarcode from 'jsbarcode';
 
 @Component({
   selector: 'app-barcode',
   templateUrl: './barcode.component.html',
   styleUrls: ['./barcode.component.scss']
 })
-export class BarcodeComponent implements OnInit {
+export class BarcodeComponent implements OnInit, AfterViewInit, AfterViewChecked {
 
   constructor() { }
 
   ngOnInit() {
     JsBarcode('#fixedBarcode', 'fixedBarcode', {
+      width: 3,
+    });
+  }
+
+  ngAfterViewInit() {
+    JsBarcode('#specifiedBarcode', 'specifiedBarcode', {
+      width: 3,
+    });
+  }
+
+  ngAfterViewChecked() {
+    JsBarcode('#interactiveBarcode', 'interactiveBarcode', {
       width: 3,
     });
   }
