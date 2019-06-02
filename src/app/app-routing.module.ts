ccaa4e72006884622870dec6e3725ddb71ab986d
ngmt <ngmt83ngmt@gmail.com>
2019-06-02 16:30:41 +0900

    BarcodeComponentへの導線を追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BarcodeComponent } from './barcode/barcode.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'barcode', component: BarcodeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
