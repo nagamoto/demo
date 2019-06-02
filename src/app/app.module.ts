f32771100e9cc891556368885058b7e0f44aecf9
ngmt <ngmt83ngmt@gmail.com>
2019-06-02 10:42:21 +0900

    CMD: ng g component barcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarcodeComponent } from './barcode/barcode.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarcodeComponent
   ],
   imports: [
     BrowserModule,
